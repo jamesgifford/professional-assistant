8af4242e7fa122aa81abb3ccd17543c3ea4d6bb2
James <james@jamesgifford.com>
2026-03-20 02:16:03 -0700

    Meta tag housekeeping

--- resources/views/components/layouts/public.blade.php
+++ resources/views/components/layouts/public.blade.php
@@ -1,14 +1,44 @@
 @props(['title' => null, 'metaDescription' => null])
 
+@php
+    $pageTitle = $title ? "$title | " . config('app.name') : config('app.name');
+    $ogImage = asset('og-image.png');
+    $canonicalUrl = url()->current();
+@endphp
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{ $title ? "$title | " . config('app.name') : config('app.name') }}</title>
+        <title>{{ $pageTitle }}</title>
         @if($metaDescription)
             <meta name="description" content="{{ $metaDescription }}">
         @endif
+        <link rel="canonical" href="{{ $canonicalUrl }}">
+
+        {{-- Open Graph --}}
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="{{ $pageTitle }}">
+        @if($metaDescription)
+            <meta property="og:description" content="{{ $metaDescription }}">
+        @endif
+        <meta property="og:url" content="{{ $canonicalUrl }}">
+        <meta property="og:image" content="{{ $ogImage }}">
+        <meta property="og:site_name" content="{{ config('app.name') }}">
+
+        {{-- Twitter Card --}}
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="{{ $pageTitle }}">
+        @if($metaDescription)
+            <meta name="twitter:description" content="{{ $metaDescription }}">
+        @endif
+        <meta name="twitter:image" content="{{ $ogImage }}">
+
+        {{-- Favicons --}}
+        <link rel="icon" href="/favicon.ico" sizes="any">
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600|jetbrains-mono:400,500" rel="stylesheet" />
